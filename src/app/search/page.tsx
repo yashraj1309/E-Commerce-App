0fd0d99e44965280db5606ffd905e7edd0ab2467
yashraj dudhatra <dudhatrayashraj@gmail.com>
2024-03-09 22:20:12 +0530

    1.4 Search Feature Done

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 import { useState, useEffect, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
+import Spinner from "react-bootstrap/Spinner";
+import { Product } from "@/types/ProductType";
 import axios from "axios";
+
+import ProductCard from "@/components/ProductCard";
 
 const getProductList = async (query: string) => {
   const response = await axios.get(
@@ -15,26 +19,73 @@ const getProductList = async (query: string) => {
 export default function Searched() {
   const searchParams = useSearchParams();
   //   getting search requested by user : q is query param
+  //example route : /search?q="iphone"
   const search = searchParams.get("q");
-  console.log("search: "+search);
+  console.log("search: " + search);
 
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
   const [noProductFound, setNoProductFount] = useState(false);
 
-   useEffect(() => {
-     const fetchSearchItems = async (query: string) => {
-       try {
-         const getProducts = await getProductList(query);
-         setProducts(getProducts);
-       } catch (error) {
-         console.error("Error fetching search items:", error);
-       }
-     };
+  useEffect(() => {
+    const fetchSearchItems = async (query: string) => {
+      try {
+        setLoading(true);
+        const getProducts = await getProductList(query);
+        setLoading(false);
+        setProducts(getProducts);
+        getProducts.length === 0 && setNoProductFount(true);
+      } catch (error) {
+        console.error("Error fetching search items:", error);
+      }
+    };
 
-     if (search) {
-       fetchSearchItems(search);
-     }
-   }, [search]);
+    if (search) {
+      fetchSearchItems(search);
+    }
+  }, [search]);
 
-  return <div className="m-5">{(products.length === 0 && noProductFound)? "Loading..." : products.length}</div>;
+  return (
+    <div
+      className="p-4"
+      style={{
+        backgroundColor: "white",
+        margin: "4.5rem 1rem 3rem 1rem",
+        borderRadius: "4px",
+      }}
+    >
+      {loading && (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
+      {products.length === 0 && !loading
+        ? "No Products available..."
+        : !loading && (
+            <div
+              style={{
+                display: "flex",
+                alignContent: "center",
+                justifyContent:"flex-start",
+                gap: "1.8rem",
+                flexWrap: "wrap",
+              }}
+            >
+              {products.map((product) => (
+                <ProductCard product={product} key={product.id} />
+              ))}
+            </div>
+          )}
+    </div>
+  );
 }
